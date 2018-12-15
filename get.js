2fe0c4fcb3558e174b35d137ceecb05a4e96500b
Oleh Makarov <gstancox@gmail.com>
2018-12-15 16:53:58 +0200

    init commit

---
+++ get.js
@@ -0,0 +1,28 @@
+const AWS = require('aws-sdk')
+const dynamoDB = new AWS.DynamoDB.DocumentClient()
+
+module.exports.run = async (event, context, callback) => {
+  const data = JSON.parse(event.body)
+
+  const params = {
+    TableName: 'todos',
+    Key: {
+      id: event.pathParameters.id
+    }
+  }
+
+  const result = await dynamoDB.get(params).promise();
+  console.log('result: ', result)
+
+  if(result.Item) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result.Item)
+    }
+  } else {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({message: "Couldn't find the todo item."})
+    }
+  }
+}
